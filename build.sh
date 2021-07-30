1161ea1135ff87cb0c6b4d2fb99fc40c74a9b618
amaris02 <kaiwen98@gmail.com>
2021-07-30 17:32:30 +0800

    Works with icon

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 SERVER="kaiwen98"
 IMAGE_NAME="moodle-avatarai"
 cat ~/my_password.txt | sudo docker login --username kaiwen98 --password-stdin
-docker build --target python -t $SERVER/$IMAGE_NAME:latest --no-cache .
+docker build --target python -t $SERVER/$IMAGE_NAME:latest --no-cache .Dockerfile-GPU
 docker push $SERVER/$IMAGE_NAME:latest
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose-development.yml down --remove-orphans
-docker-compose -f docker-compose-development.yml up --build
-
+source ~/Amaris_AI/avatarai/env/bin/activate
+cd ~/Amaris_AI/avatarai/src/
 #python app.py
+uvicorn app:app --reload --port 16000 --host 0.0.0.0 
